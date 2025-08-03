c80024bf63215f64572537ed02fa7770e8fbcc0e
Neomi Averbach <6739852@gmail.com>
2025-08-03 17:43:16 +0300

    Add LeetCode solution: climbing stairs

---
+++ climbing-stairs.py
@@ -0,0 +1,12 @@
+/**
+ * LeetCode Problem: climbing stairs
+ * Language: Python3
+ * Date: 8/3/2025 at 05:43 PM
+ * URL: https://leetcode.com/problems/climbing-stairs/submissions/1721993207/
+ * 
+ * Solution:
+ */
+
+            a, b = b, a + b
+        return b
+        
