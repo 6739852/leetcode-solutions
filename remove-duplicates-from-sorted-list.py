8c791ce67edc7b7e931e4b739bd7bec00ffae3eb
Neomi Averbach <6739852@gmail.com>
2025-08-03 20:26:24 +0300

    Add LeetCode solution: remove duplicates from sorted list

---
+++ remove-duplicates-from-sorted-list.py
@@ -0,0 +1,14 @@
+/**
+ * LeetCode Problem: remove duplicates from sorted list
+ * Language: Python3
+ * Date: 8/3/2025 at 08:26 PM
+ * URL: https://leetcode.com/problems/remove-duplicates-from-sorted-list/submissions/1722169431/
+ * 
+ * Solution:
+ */
+
+          }
+        }
+        return head;
+    }
+};
