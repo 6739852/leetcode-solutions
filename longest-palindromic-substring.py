ca9ecdbd6818884dd79f2e7ed271f56e2ccd9602
Neomi Averbach <6739852@gmail.com>
2025-08-03 16:14:15 +0300

    Add LeetCode solution: Longest Palindromic Substring

---
+++ longest-palindromic-substring.py
@@ -0,0 +1,16 @@
+/**
+ * LeetCode Problem: Longest Palindromic Substring
+ * Language: Python
+ * Date: 8/3/2025 at 04:14 PM
+ * URL: https://leetcode.com/problems/longest-palindromic-substring/description/
+ * 
+ * Solution:
+ */
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
