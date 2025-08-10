c1ad5d0aabf75235c602ed9a42a721b7e20c8d57
Neomi Averbach <6739852@gmail.com>
2025-08-11 01:27:44 +0300

    Add LeetCode solution: excel sheet column title

---
+++ excel-sheet-column-title.py
@@ -0,0 +1,15 @@
+/**
+ * LeetCode Problem: excel sheet column title
+ * Language: Python3
+ * Date: 8/11/2025 at 01:27 AM
+ * URL: https://leetcode.com/problems/excel-sheet-column-title/description/
+ * 
+ * Solution:
+ */
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+    }
+};
